ea2bfdbd536845c5767f1f4d1e38f674001307a0
yanhong wang <yanhongwang@yanhongs-MacBook-Pro.local>
2021-01-27 11:48:10 -0800

    update the API key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "sqp5y0G7ApQ32cQGXYwTghMbj9b38L2y";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
